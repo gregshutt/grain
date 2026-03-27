7ad01648a8bfc81c19982c27f6c73e8534c9359a
Greg Shutt <github@gregshutt.com>
2026-03-27 09:56:21 -0400

    fixing bug in enviro

--- config/settings.py
+++ config/settings.py
@@ -2,13 +2,13 @@ import os
 
 from distutils.util import strtobool
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 SERVER_NAME = os.getenv(
     "SERVER_NAME", "localhost:{0}".format(os.getenv("PORT", "8000"))
 )
 
-MINIO_HOST = os.environ["MINIO_HOST"]
-MINIO_ACCESS_KEY = os.environ["MINIO_ACCESS_KEY"]
-MINIO_SECRET_KEY = os.environ["MINIO_SECRET_KEY"]
-MINIO_BUCKET = os.environ["MINIO_BUCKET"]
+MINIO_HOST = os.getenv("MINIO_HOST", "")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "")
+MINIO_BUCKET = os.getenv("MINIO_BUCKET", "")
